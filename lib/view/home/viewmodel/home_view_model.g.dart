d3853ba4de51944b71fde7012431f813d12a0441
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-04-22 09:19:01 +0300

    g.dart creared

--- lib/view/home/viewmodel/home_view_model.g.dart
+++ lib/view/home/viewmodel/home_view_model.g.dart
@@ -9,10 +9,39 @@ part of 'home_view_model.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$HomeViewModel on _HomeViewModelBase, Store {
+  final _$dateAtom = Atom(name: '_HomeViewModelBase.date');
+
+  @override
+  DateTime get date {
+    _$dateAtom.reportRead();
+    return super.date;
+  }
+
+  @override
+  set date(DateTime value) {
+    _$dateAtom.reportWrite(value, super.date, () {
+      super.date = value;
+    });
+  }
+
+  final _$_HomeViewModelBaseActionController =
+      ActionController(name: '_HomeViewModelBase');
+
+  @override
+  String getDate() {
+    final _$actionInfo = _$_HomeViewModelBaseActionController.startAction(
+        name: '_HomeViewModelBase.getDate');
+    try {
+      return super.getDate();
+    } finally {
+      _$_HomeViewModelBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+date: ${date}
     ''';
   }
 }

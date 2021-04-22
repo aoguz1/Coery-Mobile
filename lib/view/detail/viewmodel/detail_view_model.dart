165480d5ee5e298c36cb76a0ef81339819e020ba
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-04-22 09:18:28 +0300

    detail view_model created

---
+++ lib/view/detail/viewmodel/detail_view_model.dart
@@ -0,0 +1,6 @@
+import 'package:mobx/mobx.dart';
+part 'detail_view_model.g.dart';
+
+class DetailViewModel = _DetailViewModelBase with _$DetailViewModel;
+
+abstract class _DetailViewModelBase with Store {}

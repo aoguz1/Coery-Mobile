ce4c2f9d74388c60e9312ada3d596b213ac29352
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-04-22 09:18:47 +0300

    home view written new features

--- lib/view/home/view/home_view.dart
+++ lib/view/home/view/home_view.dart
@@ -15,72 +15,208 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: context.colors.onPrimary,
+        child: Icon(
+          Icons.add,
+          color: context.colors.background,
+          size: 30,
+        ),
+      ),
+      body: Stack(
         children: [
-          Expanded(
-            flex: 4,
+          SizedBox(
             child: Container(
-              padding: context.paddingMedium,
-              width: double.infinity,
-              decoration: BoxDecoration(color: context.colors.primary),
+              color: context.colors.primary,
+            ),
+            height: context.mediaQuery.size.height * 0.4,
+          ),
+          Container(
+            padding: context.paddingMedium,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(flex: 1, child: SizedBox()),
+                Container(
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(profile_photo_url))),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        _viewModel.getDate(),
+                        style: context.textTheme.button
+                            .copyWith(color: Color(ColorConstants.SILVER_RUST)),
+                      )),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'İyi Günler Abdullah 🙌',
+                        style: context.textTheme.headline5,
+                      )),
+                ),
+                Spacer(
+                  flex: 10,
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: context.mediaQuery.size.height * 0.310,
+            left: context.mediaQuery.size.width * 0.06,
+            child: Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.4),
+                  spreadRadius: 0.01,
+                  blurRadius: 14,
+                  offset: Offset(0, 0.1), // changes position of shadow
+                ),
+              ], color: Colors.white, borderRadius: BorderRadius.circular(22)),
+              width: context.mediaQuery.size.width * 0.42,
+              height: context.mediaQuery.size.height * 0.24,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Expanded(flex: 1, child: SizedBox()),
                   Container(
-                    child: Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(profile_photo_url))),
+                    padding: context.paddingNormal,
+                    child: Text(
+                      '⏰ Yapılacaklar',
+                      style: context.textTheme.bodyText1,
                     ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          _viewModel.getDate(),
-                          style: context.textTheme.button.copyWith(
-                              color: Color(ColorConstants.SILVER_RUST)),
-                        )),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'İyi Günler Abdullah 🙌',
-                          style: context.textTheme.headline5,
-                        )),
-                  ),
-                  Spacer(
-                    flex: 2,
                   )
                 ],
               ),
             ),
           ),
-          Expanded(
-              flex: 6,
-              child: Stack(
+          Positioned(
+            top: context.mediaQuery.size.height * 0.570,
+            left: context.mediaQuery.size.width * 0.06,
+            child: Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.4),
+                  spreadRadius: 0.01,
+                  blurRadius: 14,
+                  offset: Offset(0, 0.1), // changes position of shadow
+                ),
+              ], color: Colors.white, borderRadius: BorderRadius.circular(22)),
+              width: context.mediaQuery.size.width * 0.42,
+              height: context.mediaQuery.size.height * 0.24,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Positioned(
-                      top: 20,
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 200,
-                            height: 200,
-                            color: Colors.amber,
-                          )
-                        ],
-                      ))
+                  Container(
+                    padding: context.paddingNormal,
+                    child: Text(
+                      '💦 Su',
+                      style: context.textTheme.bodyText1,
+                    ),
+                  )
                 ],
-              ))
+              ),
+            ),
+          ),
+          Positioned(
+            right: context.mediaQuery.size.height * 0.03,
+            top: context.mediaQuery.size.height * 0.310,
+            child: Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.4),
+                  spreadRadius: 0.01,
+                  blurRadius: 14,
+                  offset: Offset(0, 0.1), // changes position of shadow
+                ),
+              ], color: Colors.white, borderRadius: BorderRadius.circular(22)),
+              width: context.mediaQuery.size.width * 0.42,
+              padding: EdgeInsets.all(0),
+              height: context.mediaQuery.size.height * 0.20,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: context.paddingNormal,
+                    child: Text(
+                      '📕 Kitap',
+                      style: context.textTheme.bodyText1,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            right: context.mediaQuery.size.height * 0.03,
+            top: context.mediaQuery.size.height * 0.53,
+            child: Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.4),
+                  spreadRadius: 0.01,
+                  blurRadius: 14,
+                  offset: Offset(0, 0.1), // changes position of shadow
+                ),
+              ], color: Colors.white, borderRadius: BorderRadius.circular(22)),
+              width: context.mediaQuery.size.width * 0.42,
+              height: context.mediaQuery.size.height * 0.20,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: context.paddingNormal,
+                    child: Text(
+                      '💪 Spor',
+                      style: context.textTheme.bodyText1,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          DraggableScrollableSheet(
+              initialChildSize: 0.165,
+              minChildSize: 0.165,
+              maxChildSize: 0.71,
+              builder: (context, scrollController) {
+                return Container(
+                  child: ListView.builder(
+                      controller: scrollController,
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text('index $index'),
+                        );
+                      }),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.4),
+                          spreadRadius: 0.01,
+                          blurRadius: 14,
+                          offset: Offset(0, 0.1), // changes position of shadow
+                        ),
+                      ],
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(25),
+                          topRight: Radius.circular(25))),
+                );
+              })
         ],
       ),
     );

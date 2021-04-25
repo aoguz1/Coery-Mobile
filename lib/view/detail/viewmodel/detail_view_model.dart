dbf07679b319bf40c1fcac295fc90c53e7700d1c
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-04-25 16:04:23 +0300

    detail view model written

--- lib/view/detail/viewmodel/detail_view_model.dart
+++ lib/view/detail/viewmodel/detail_view_model.dart
@@ -1,6 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:fluttermvvmtemplate/core/base/model/base_view_model.dart';
 import 'package:mobx/mobx.dart';
 part 'detail_view_model.g.dart';
 
 class DetailViewModel = _DetailViewModelBase with _$DetailViewModel;
 
-abstract class _DetailViewModelBase with Store {}
+abstract class _DetailViewModelBase with Store, BaseViewModel {
+  var controller = PageController(initialPage: 1, viewportFraction: 0.3);
+
+  @observable
+  int initialPage = 1;
+
+  @action
+  changeInitialPage(int value) {
+    initialPage = value;
+  }
+
+  @override
+  void setContext(BuildContext context) => this.context = context;
+  void init() {}
+}
--- lib/view/detail/viewmodel/detail_view_model.g.dart
+++ lib/view/detail/viewmodel/detail_view_model.g.dart
@@ -9,10 +9,39 @@ part of 'detail_view_model.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$DetailViewModel on _DetailViewModelBase, Store {
+  final _$initialPageAtom = Atom(name: '_DetailViewModelBase.initialPage');
+
+  @override
+  int get initialPage {
+    _$initialPageAtom.reportRead();
+    return super.initialPage;
+  }
+
+  @override
+  set initialPage(int value) {
+    _$initialPageAtom.reportWrite(value, super.initialPage, () {
+      super.initialPage = value;
+    });
+  }
+
+  final _$_DetailViewModelBaseActionController =
+      ActionController(name: '_DetailViewModelBase');
+
+  @override
+  dynamic changeInitialPage(int value) {
+    final _$actionInfo = _$_DetailViewModelBaseActionController.startAction(
+        name: '_DetailViewModelBase.changeInitialPage');
+    try {
+      return super.changeInitialPage(value);
+    } finally {
+      _$_DetailViewModelBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+initialPage: ${initialPage}
     ''';
   }
 }

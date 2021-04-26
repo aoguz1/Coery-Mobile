4ec477ccaf9f7b860256b28ea9bf0fd1f9dbaa84
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-04-26 09:14:17 +0300

    detail view widget written

--- lib/view/detail/view/detail_view.dart
+++ lib/view/detail/view/detail_view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fluttermvvmtemplate/core/base/view/base_widget.dart';
+import 'package:fluttermvvmtemplate/core/components/button/water_button.dart';
 import 'package:fluttermvvmtemplate/core/components/page_view_card/page_view_card.dart';
+import 'package:fluttermvvmtemplate/core/components/text_input/text_input.dart';
+import 'package:fluttermvvmtemplate/core/components/to_do_component/to_do_square.dart';
 import 'package:fluttermvvmtemplate/core/extension/context_extension.dart';
 import 'package:fluttermvvmtemplate/view/detail/viewmodel/detail_view_model.dart';
 
@@ -81,7 +85,6 @@ class DetailView extends StatelessWidget {
                                         child: Container(
                                           child: PageView(
                                             onPageChanged: (value) {
-                                              print(value);
                                               viewModel
                                                   .changeInitialPage(value);
                                             },
@@ -105,9 +108,9 @@ class DetailView extends StatelessWidget {
                             ),
                             Expanded(
                               flex: 5,
-                              child: Container(
-                                color: Colors.brown,
-                              ),
+                              child: Observer(builder: (_) {
+                                return viewModel.changePage();
+                              }),
                             ),
                             Spacer(
                               flex: 6,
@@ -118,5 +121,131 @@ class DetailView extends StatelessWidget {
                 ],
               ),
             ));
+  }
+}
+
+class BookDetailWidget extends StatelessWidget {
+  BookDetailWidget({
+    Key key,
+  }) : super(key: key);
+  final viewModel = DetailViewModel();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          TextInputWidget('Kitap Adı', viewModel.book_name_text_controller),
+          TextInputWidget('Okunan Sayfa', viewModel.book_name_text_controller)
+        ],
+      ),
+    );
+  }
+}
+
+class SportDetailWidget extends StatelessWidget {
+  SportDetailWidget({
+    Key key,
+  }) : super(key: key);
+  final viewModel = DetailViewModel();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          TextInputWidget('Aktivite Adı', viewModel.book_name_text_controller),
+          TextInputWidget('Süre', viewModel.book_name_text_controller)
+        ],
+      ),
+    );
+  }
+}
+
+class TodoDetailWidget extends StatelessWidget {
+  TodoDetailWidget({
+    Key key,
+  }) : super(key: key);
+  final viewModel = DetailViewModel();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: context.paddingLow,
+      child: Column(
+        children: [
+          Expanded(
+              flex: 2,
+              child: TextInputWidget(
+                  'Aktivite Adı', viewModel.book_name_text_controller)),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: context.paddingLow,
+              child: Row(
+                children: [
+                  Flexible(child: ToDoSquare(context.colors.primary)),
+                  Flexible(child: ToDoSquare(context.colors.error)),
+                  Flexible(child: ToDoSquare(context.colors.secondary)),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class WaterDetailWidget extends StatelessWidget {
+  WaterDetailWidget({
+    Key key,
+  }) : super(key: key);
+  final viewModel = DetailViewModel();
+  @override
+  Widget build(BuildContext context) {
+    String glass_svg = 'asset/image/glass.png';
+    String mini_bottle_svg = 'asset/image/bottle1.png';
+    String big_bottle_svg = 'asset/image/bottle2.png';
+    return Container(
+      padding: context.paddingLow,
+      child: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: context.paddingMediumHorizontal,
+              child: RichText(
+                text: TextSpan(children: [
+                  TextSpan(
+                      text: 'Bugünkü ihtiyacını karşılamak için',
+                      style: context.textTheme.subtitle2),
+                  TextSpan(
+                    text: ' 1.4',
+                    style: context.textTheme.subtitle2
+                        .copyWith(color: context.colors.primary),
+                  ),
+                  TextSpan(
+                    text: ' litre daha su içmelisin',
+                    style: context.textTheme.subtitle2,
+                  )
+                ]),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Padding(
+              padding: context.paddingNormalVertical.copyWith(bottom: 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  WaterButton(glass_svg, '200 ml', () {}),
+                  WaterButton(mini_bottle_svg, '500 ml', () {}),
+                  WaterButton(big_bottle_svg, '1 lt', () {})
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }

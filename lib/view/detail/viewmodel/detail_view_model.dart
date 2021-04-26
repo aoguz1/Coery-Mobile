d7e3be82c7b225829b5093fbf8719764d202926f
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-04-26 09:14:47 +0300

    detail_view viewmodel change page function written

--- lib/view/detail/viewmodel/detail_view_model.dart
+++ lib/view/detail/viewmodel/detail_view_model.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:fluttermvvmtemplate/core/base/model/base_view_model.dart';
+import 'package:fluttermvvmtemplate/view/detail/view/detail_view.dart';
 import 'package:mobx/mobx.dart';
 part 'detail_view_model.g.dart';
 
 class DetailViewModel = _DetailViewModelBase with _$DetailViewModel;
 
 abstract class _DetailViewModelBase with Store, BaseViewModel {
-  var controller = PageController(initialPage: 1, viewportFraction: 0.3);
+  var book_name_text_controller = TextEditingController();
+  var read_page_text_controller = TextEditingController();
+  var controller = PageController(initialPage: 0, viewportFraction: 0.3);
 
   @observable
-  int initialPage = 1;
+  int initialPage = 0;
 
   @action
   changeInitialPage(int value) {
     initialPage = value;
+  }
+
+  changePage() {
+    switch (initialPage) {
+      case 0:
+        return SportDetailWidget();
+        break;
+      case 1:
+        return WaterDetailWidget();
+        break;
+      case 2:
+        return BookDetailWidget();
+        break;
+      case 3:
+        return TodoDetailWidget();
+        break;
+      default:
+    }
   }
 
   @override
